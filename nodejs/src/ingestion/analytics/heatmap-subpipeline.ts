PostHog/posthog

refactor: move batch stores into inputs

nodejs/src/ingestion/analytics/heatmap-subpipeline.ts
@@ -1,14 +1,13 @@
 import { PluginEvent } from '~/plugin-scaffold'
 
-import { KafkaProducerWrapper } from '../../kafka/producer'
 import { EventHeaders, Team } from '../../types'
 import { TeamManager } from '../../utils/team-manager'
 import { GroupTypeManager } from '../../worker/ingestion/group-type-manager'
-import { BatchWritingGroupStore } from '../../worker/ingestion/groups/batch-writing-group-store'
 import { createCheckHeatmapOptInStep } from '../event-processing/check-heatmap-opt-in-step'
 import { createDisablePersonProcessingStep } from '../event-processing/disable-person-processing-step'
 import { EventPipelineRunnerOptions } from '../event-processing/event-pipeline-options'
 import { createExtractHeatmapDataStep } from '../event-processing/extract-heatmap-data-step'
+import { BatchStores } from '../event-processing/flush-batch-stores-step'
 import { createNormalizeEventStep } from '../event-processing/normalize-event-step'
 import { createPrepareEventStep } from '../event-processing/prepare-event-step'
 import { createSkipEmitEventStep } from '../event-processing/skip-emit-event-step'
@@ -26,24 +25,21 @@ export interface HeatmapSubpipelineConfig {
     }
     teamManager: TeamManager
     groupTypeManager: GroupTypeManager
-    groupStore: BatchWritingGroupStore
-    kafkaProducer: KafkaProducerWrapper
 }
 
-export function createHeatmapSubpipeline<TInput extends HeatmapSubpipelineInput, TContext>(
+export function createHeatmapSubpipeline<TInput extends HeatmapSubpipelineInput & BatchStores, TContext>(
     builder: StartPipelineBuilder<TInput, TContext>,
     config: HeatmapSubpipelineConfig
 ): PipelineBuilder<TInput, void, TContext> {
-    const { options, teamManager, groupTypeManager, groupStore, kafkaProducer } = config
+    const { options, teamManager, groupTypeManager } = config
 
     return builder
         .pipe(createCheckHeatmapOptInStep())
         .pipe(createDisablePersonProcessingStep())
         .pipe(createNormalizeEventStep())
-        .pipe(createPrepareEventStep(teamManager, groupTypeManager, groupStore, options))
+        .pipe(createPrepareEventStep(teamManager, groupTypeManager, options))
         .pipe(
             createExtractHeatmapDataStep({
-                kafkaProducer,
                 CLICKHOUSE_HEATMAPS_KAFKA_TOPIC: options.CLICKHOUSE_HEATMAPS_KAFKA_TOPIC,
             })
         )

BlackRoad-OS-Inc/RoadHog

nodejs/src/ingestion/analytics/heatmap-subpipeline.ts
@@ -1,14 +1,13 @@
 import { PluginEvent } from '~/plugin-scaffold'
 
-import { KafkaProducerWrapper } from '../../kafka/producer'
 import { EventHeaders, Team } from '../../types'
 import { TeamManager } from '../../utils/team-manager'
 import { GroupTypeManager } from '../../worker/ingestion/group-type-manager'
-import { BatchWritingGroupStore } from '../../worker/ingestion/groups/batch-writing-group-store'
 import { createCheckHeatmapOptInStep } from '../event-processing/check-heatmap-opt-in-step'
 import { createDisablePersonProcessingStep } from '../event-processing/disable-person-processing-step'
 import { EventPipelineRunnerOptions } from '../event-processing/event-pipeline-options'
 import { createExtractHeatmapDataStep } from '../event-processing/extract-heatmap-data-step'
+import { BatchStores } from '../event-processing/flush-batch-stores-step'
 import { createNormalizeEventStep } from '../event-processing/normalize-event-step'
 import { createPrepareEventStep } from '../event-processing/prepare-event-step'
 import { createProcessGroupsStep } from '../event-processing/process-groups-step'
@@ -27,25 +26,22 @@ export interface HeatmapSubpipelineConfig {
     }
     teamManager: TeamManager
     groupTypeManager: GroupTypeManager
-    groupStore: BatchWritingGroupStore
-    kafkaProducer: KafkaProducerWrapper
 }
 
-export function createHeatmapSubpipeline<TInput extends HeatmapSubpipelineInput, TContext>(
+export function createHeatmapSubpipeline<TInput extends HeatmapSubpipelineInput & BatchStores, TContext>(
     builder: StartPipelineBuilder<TInput, TContext>,
     config: HeatmapSubpipelineConfig
 ): PipelineBuilder<TInput, void, TContext> {
-    const { options, teamManager, groupTypeManager, groupStore, kafkaProducer } = config
+    const { options, teamManager, groupTypeManager } = config
 
     return builder
         .pipe(createCheckHeatmapOptInStep())
         .pipe(createDisablePersonProcessingStep())
         .pipe(createNormalizeEventStep())
         .pipe(createPrepareEventStep())
-        .pipe(createProcessGroupsStep(teamManager, groupTypeManager, groupStore, options))
+        .pipe(createProcessGroupsStep(teamManager, groupTypeManager, options))
         .pipe(
             createExtractHeatmapDataStep({
-                kafkaProducer,
                 CLICKHOUSE_HEATMAPS_KAFKA_TOPIC: options.CLICKHOUSE_HEATMAPS_KAFKA_TOPIC,
             })
         )